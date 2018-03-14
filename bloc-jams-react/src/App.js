17ff1c5f316ac84582fd13b0b17a6e83cb342054
0xZeb <zebsmithson@yahoo.com>
2018-03-14 14:36:22 -0700

    Album routing checkpoint

--- bloc-jams-react/src/App.js
+++ bloc-jams-react/src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 
 class App extends Component {
@@ -13,11 +14,13 @@ class App extends Component {
         <header>
           <Link to='/'>Landing</Link>
           <Link to='/library'>Library</Link>
+
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+        
         </main>
       </div>
     );
