9db9b7c1f5017ca385692601a921c93247ee3bf7
Zeb Smithson <zebsmithson@yahoo.com>
2018-04-02 16:55:37 -0700

    progress slider borked, time is formatted

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -16,7 +16,7 @@ class Album extends Component {
       album: album,
       currentSong: album.songs[0],
       currentTime: 0,
-      duration: album.songs[0].duraction,
+      duration: album.songs[0].duration,
       isPlaying: false
     };
 
@@ -80,7 +80,7 @@ class Album extends Component {
   componentDidMount() {
     this.eventListeners = {
       timeupdate: e => {
-      this.setState({ currentTime: this.audioElement.currentTime });
+      this.setState({ currentTime: this.audioElement.currentTime  });
     },
     durationchange: e => {
       this.setState({ duration: this.audioElement.duration });
@@ -103,68 +103,57 @@ class Album extends Component {
       this.setState.currentTime = ({ currentTime: newTime });
     }
 
+    formatTime(s){
+
+      return (s-(s%=60))/60+ (9<s?':':':0')+Math.floor(s);
+
+  }
+
 
 
   render() {
-    return ( < section className = "album" > < section id = "album-info" >
-      <
-      img id = "album-cover-art"
-      src = {
-        this.state.album.albumCover
-      }
-      /> <
-      div className = "album-details" >
-      <
-      h1 id = "album-title" > {
-        this.state.album.title
-      } < /h1> <
-      h2 className = "artist" > {
-        this.state.album.artist
-      } < /h2> <
-      div id = "release-info" > {
-        this.state.album.releaseInfo
-      } < /div> < /
-      div > <
-      /section> <
-      table id = "song-list" >
-      <
-      colgroup >
-      <
-      col id = "song-number-column" / >
-      <
-      col id = "song-title-column" / >
-      <
-      col id = "song-duration-column" / >
+    return (
+       < section className = "album" > < section id = "album-info" >
+      < img id = "album-cover-art" src = { this.state.album.albumCover }/>
+          <  div className = "album-details" >
+          < h1 id = "album-title" > { this.state.album.title} < /h1>
+           <h2 className = "artist" >
+           { this.state.album.artist } < /h2>
+            <div id = "release-info" > { this.state.album.releaseInfo} < /div>
+      < /div >
+
+       </section>
+            < table id = "song-list" >
+      <colgroup >
+            <col id = "song-number-column" / >
+            <col id = "song-title-column" / >
+            < col id = "song-duration-column" / >
       </colgroup>
+
       <tbody > { this.state.album.songs.map((song, index) =>
 
           <tr className = "song"  key={index } onClick={()=>this.handleSongClick(song)}>
 
           <td className = "song-actions" >
-          < button >
-          < span className = "song-number" >
-           {index + 1 }
-             < /span>
-          < span className = "ion-play" > < /span>
-           <  span className = "ion-pause" > < /span>
-           < / button >
-            < /td>
-          < td className = "song-title" >
-              {song.title }
-           < /td>
-          <td className = "song-duration" > { song.duration  }
-            < /td>
+            < button >
+            < span className = "song-number" > {index + 1 }< /span>
+            < span className = "ion-play" > < /span>
+            <  span className = "ion-pause" > < /span>
+            < / button >
+          < /td>
+          < td className = "song-title" > { song.title }< /td>
+           <td className = "song-duration" > { this.formatTime( song.duration ) }< /td>
           < / tr >
-        )
-      }   </tbody>
+        )}
+           </tbody>
         < /table >
 
 
         <PlayerBar
-           isPlaying={this.state.isPlaying}
-           currentSong={this.state.currentSong}
-           currentTime={this.audioElement.currentTime}
-           duration={this.audioElement.duration}
+           isPlaying={     this.state.isPlaying }
+           currentSong={   this.state.currentSong }
+           currentTime={   this.formatTime(this.audioElement.currentTime) }
+           duration={      this.formatTime(this.audioElement.duration) }
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
            handleNextClick={() => this.handleNextClick()}
--- bloc-jams-react/src/components/PlayerBar.js
+++ bloc-jams-react/src/components/PlayerBar.js
@@ -19,11 +19,11 @@ class PlayerBar extends Component {
        </section>
 
        <section id = "time-control">
-         <div className = "current-time">{this.props.currentTime}</div>
+         <div className = "current-time">{ this.props.currentTime }</div>
          <input
             type = "range"
             className = "seek-bar"
-            value = { (this.props.currentTime / this.props.duration) || 0}
+            value = {  (this.props.currentTime) + (this.props.duration) || 0}
             max = "1"
             min ="0"
             step = "0.01"
@@ -31,6 +31,7 @@ class PlayerBar extends Component {
          />
          <div className = "total-time" >{ this.props.duration }</div>
        </section>
+
 
        <section id = "volume-control">
          <div className = "icon ion-volume-low"></div>
